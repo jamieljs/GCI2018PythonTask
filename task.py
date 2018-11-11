491f5b5a7e767b6ee38e490afb7567a06f6270a2
jamieljs <44937219+jamieljs@users.noreply.github.com>
2018-11-11 13:14:55 +0800

    Create task.py

---
+++ task.py
@@ -0,0 +1,5 @@
+for i in range(10):
+	print("GCI is great")
+
+name = input("What is your name?")
+print("Hello " + name + ", please to meet you! Did you know that your name backwards is " + name[::-1] + "?")
